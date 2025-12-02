e7fac7b6428d3b1a8eb838933601c0bdcc2a02bd
Kira366 <khaitran.k366@gmail.com>
2025-12-02 11:48:47 +0700

    Update Card UI

---
+++ KatCard-Prototype/Assets/Scripts/Card.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+    [SerializeField] Image icon;
+    [SerializeField] CardData dataSO;
+    [SerializeField] Sprite hiddenSprite;
+    public bool isSelected;
+
+    [ContextMenu("Show Card")]
+    public void Show()
+    {
+        icon.sprite = dataSO.sprite;
+        isSelected = true;
+    }
+
+    [ContextMenu("Hide Card")]
+    public void Hide()
+    {
+        icon.sprite = hiddenSprite;
+        isSelected = false;
+    }
+
+}
